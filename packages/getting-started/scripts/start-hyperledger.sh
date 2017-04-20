7c3fa22c73a3fb827772de75d90a7993f8c82ec2
Matthew B White <whitemat@uk.ibm.com>
2017-04-20 17:18:12 +0100

    Add sleep to permit Fabric to start

--- packages/getting-started/scripts/start-hyperledger.sh
+++ packages/getting-started/scripts/start-hyperledger.sh
@@ -13,6 +13,9 @@ docker-compose -f hlfv1_alpha-docker-compose.yml down
 docker rm $(docker ps -aq) ||
 docker-compose -f hlfv1_alpha-docker-compose.yml up -d
 
+# wait for Hyperledger Fabric to start 
+sleep 10
+
 node create-channel.js
 node join-channel.js
 cd ../..
