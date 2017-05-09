257441f03fbd874e47c92cec7a783de0d28f4827
Dave Kelsey <d_kelsey@uk.ibm.com>
2017-05-09 15:56:09 +0100

    fix(startup): fix issue where fabric doesn’t get started.
    Signed-off-by: Dave Kelsey <d_kelsey@uk.ibm.com>

--- packages/getting-started/scripts/start-hyperledger.sh
+++ packages/getting-started/scripts/start-hyperledger.sh
@@ -10,7 +10,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 cd "${DIR}"/hlfv1
 
 docker-compose -f docker-compose.yml down
-docker rm $(docker ps -aq) || docker-compose -f docker-compose.yml up -d
+docker rm $(docker ps -aq) > /dev/null 2>&1 || true
+docker-compose -f docker-compose.yml up -d
 
 # wait for Hyperledger Fabric to start 
 # incase of errors when running later commands, increase this value and restart
